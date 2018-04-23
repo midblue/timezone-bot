33b222a1a46b96880bc5985a17cd188888792fc1
Jasper Stephenson <jasperstephenson@gmail.com>
2018-04-23 12:11:10 +0900

    Added full support for any time zone in the world

--- index.js
+++ index.js
@@ -1,29 +1,13 @@
 require('dotenv').config()
 const Discord = require('discord.js')
 const client = new Discord.Client()
+const timeZones = require('./timezones.json')
+
+const relevantTimeZones = '!tst !pdt !cdt !edt'
 
 client.on('ready', () => {
   console.log('Ready!')
 })
-
-const timeZones = {
-  jst: {
-      timezone: 'Asia/Tokyo',
-      label: 'Japan'
-  },
-  pst: {
-      timezone: 'America/Los_Angeles',
-      label: 'US West'
-  },
-  cst: {
-      timezone: 'America/Chicago',
-      label: 'US Central'
-  },
-  est: {
-      timezone: 'America/New_York',
-      label: 'US East'
-  },
-}
 
 function buildDateForTimezone(timeZone) {
   const localeString = new Date().toLocaleTimeString(undefined, {
@@ -31,33 +15,53 @@ function buildDateForTimezone(timeZone) {
     weekday: 'short',
     hour: '2-digit',
     minute: '2-digit'
-  });
+  })
 
   const singleDigitHourRegex = / ([0-9]{1}):[0-9]+/
   return localeString.replace(singleDigitHourRegex, match => {
-      if(match) return ' 0' + match.trim();
-  });
-}
-
-function buildOutputStringForTimezone(timezoneCode) {
-    const locale = timeZones[timezoneCode];
-    if(!locale) return '';
-
-    const localTime = buildDateForTimezone(locale.timezone);
-    const label = locale.label;
-    return `\`\`\`${label}: ${localTime}\`\`\``
+      if (match) return ' 0' + match.trim()
+  })
 }
 
 function formattedTimeZones (localTimes) {
   const longest = localTimes
     .reduce((longestLabel, t) => {
-      return t.timeZone.length > longestLabel ? t.timeZone.length : longestLabel
+      return t.abbr.length + t.timeZone.length > longestLabel
+        ? t.abbr.length + t.timeZone.length : longestLabel
     }, 0)
+
   return localTimes.map(t => {
     let paddedTimeZone = t.timeZone
-    while (paddedTimeZone.length < longest) paddedTimeZone += ' '
-    return `${paddedTimeZone}  ${t.time}`;
+    while (t.abbr.length + paddedTimeZone.length < longest)
+      paddedTimeZone += ' '
+    return `(${t.abbr}) ${paddedTimeZone}  ${t.time}`
   })
+}
+
+function parseAts (messageText) {
+  let atsInMessage = []
+  const atRegex = /\<\@\!?(\d*)\>*/g
+  let regexedAts = atRegex.exec(messageText)
+  while (regexedAts) {
+    atsInMessage.push(regexedAts[1])
+    regexedAts = atRegex.exec(messageText)
+  }
+  return atsInMessage
+}
+
+function parseTimeZoneCommands (messageText) {
+  let timeZonesInMessage = []
+  const lowerCaseMessage = messageText.toLowerCase()
+  const timeZoneRegex = /!([a-z]{2,4})/g
+  let regexedTimeZone = timeZoneRegex.exec(lowerCaseMessage)
+  while (regexedTimeZone) {
+    const foundTimeZone = timeZones
+      .find(t => t.abbr.toLowerCase() === regexedTimeZone[1])
+    if (foundTimeZone)
+      timeZonesInMessage.push(foundTimeZone)
+    regexedTimeZone = timeZoneRegex.exec(lowerCaseMessage)
+  }
+  return timeZonesInMessage
 }
 
 client.on('message', msg => {
@@ -68,41 +72,30 @@ client.on('message', msg => {
     bot: msg.author.bot,
   }
   // console.log(usefulInfo)
-  let atsInMessage = []
-  const atRegex = /\<\@\!?(\d*)\>*/g
-  let regexedAts = atRegex.exec(msg.content)
-  while (regexedAts) {
-    atsInMessage.push(regexedAts[1])
-    regexedAts = atRegex.exec(msg.content)
-  }
-  if (atsInMessage.length > 0) console.log(atsInMessage)
 
-  if (msg.content.indexOf('!jst') >= 0) {
-    msg.channel.send(buildOutputStringForTimezone('jst'))
-  }
+  const atsInMessage = parseAts(msg.content)
+  if (atsInMessage.length > 0)
+    console.log(atsInMessage)
 
-  if (msg.content.indexOf('!pst') >= 0) {
-    msg.channel.send(buildOutputStringForTimezone('pst'))
-  }
+  const timeZonesInMessage = (msg.content.indexOf('!all') >= 0)
+    ? parseTimeZoneCommands(relevantTimeZones)
+    : parseTimeZoneCommands(msg.content)
 
-  if (msg.content.indexOf('!cst') >= 0) {
-    msg.channel.send(buildOutputStringForTimezone('cst') + ' :ok_hand:')
-  }
+  if (timeZonesInMessage.length === 0) return
 
-  if (msg.content.indexOf('!est') >= 0) {
-    msg.channel.send(buildOutputStringForTimezone('est'))
-  }
+  const localTimes = timeZonesInMessage.map(zone => {
+    const localTime = buildDateForTimezone(
+      zone.utc.find(u => u.indexOf('Etc/GMT') === -1)
+    )
+    return {
+      abbr: zone.abbr,
+      timeZone: zone.value.substring(0, zone.value.indexOf('Standard Time') - 1),
+      time: localTime
+    }
+  })
 
-  if(msg.content === '!all') {
-    const localTimes = Object.keys(timeZones).map(zone => {
-      const localTime = buildDateForTimezone(timeZones[zone].timezone)
-      return {
-        timeZone: timeZones[zone].label,
-        time: localTime
-      }
-    })
-    msg.channel.send(`\`\`\`${formattedTimeZones(localTimes).join(`\n`)}\`\`\``)
-  }
+  msg.channel.send(`\`\`\`${formattedTimeZones(localTimes).join(`\n`)}\`\`\``)
+
 })
 
 client.login(process.env.DISCORD_KEY)
