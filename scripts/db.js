ed9bfbb90044ac10b3fbb5cb0086f300d849c42b
Jasper Stephenson <jasperstephenson@gmail.com>
2018-05-05 22:09:28 +0900

    Revert "Monster commit: removed local file storage and added MySQL and Redis storage, which meant a rewrite of most of the codebase"
    This reverts commit 710be10d83b0c433e74d0476465927cfd4375e15.

--- scripts/db.js
+++ scripts/db.js
@@ -1,199 +1,55 @@
-const logger = require('./log.js')
-const log = logger('db', 'yellow')
-const err = logger('db', 'red')
-const debug = logger('db', 'yellow', true)
+const fs = require('fs')
 
-/*
+let savedUsersFileExists = true
+try { require.resolve('../data/users.json') }
+catch(e) { savedUsersFileExists = false }
+const savedUsers = savedUsersFileExists ? require('../data/users.json') : {}
 
-Hi! Our SQL database schema looks like this:
-
-place
-+-------+-------------+------+-----+---------+-------+
-| Field | Type        | Null | Key | Default | Extra |
-+-------+-------------+------+-----+---------+-------+
-| name  | varchar(50) | NO   | PRI |         |       |
-| lat   | float       | YES  |     | NULL    |       |
-| lng   | float       | YES  |     | NULL    |       |
-+-------+-------------+------+-----+---------+-------+
-
-user
-+----------+-------------+------+-----+---------+-------+
-| Field    | Type        | Null | Key | Default | Extra |
-+----------+-------------+------+-----+---------+-------+
-| id       | varchar(20) | NO   | PRI | NULL    |       |
-| location | varchar(50) | YES  |     | NULL    |       |
-+----------+-------------+------+-----+---------+-------+
-
-server_user
-+----------+-------------+------+-----+---------+-------+
-| Field    | Type        | Null | Key | Default | Extra |
-+----------+-------------+------+-----+---------+-------+
-| lastSeen | datetime    | YES  |     | NULL    |       |
-| username | varchar(50) | YES  |     | NULL    |       |
-| serverId | varchar(20) | NO   | PRI |         |       |
-| userId   | varchar(20) | NO   | PRI |         |       |
-+----------+-------------+------+-----+---------+-------+
-
-*/
-
-
-const mysql = require('mysql').createConnection({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: 'timezonebot'
-})
-
-mysql.query(`SELECT * FROM user;`,
-  (e, results, fields) => {
-    if (e) err('MySQL error:', e)
-    log(`Connected to MySQL (${results.length} saved users)`)
-  })
+console.log(`Loaded ${Object.keys(savedUsers).length} saved users`)
 
 module.exports = {
-  getUser (userId, serverId) {
-    debug('getUser')
-    return new Promise ((resolve, reject) => {
-      mysql.query(
-        `SELECT server_user.username, server_user.lastSeen, user.location, place.lat, place.lng
-        FROM server_user
-        LEFT JOIN user ON user.id = '${userId}'
-        LEFT JOIN place ON place.name = user.location
-        WHERE serverId = '${serverId}' AND userId = '${userId}';`,
-        (e, results, fields) => {
-          if (e) return err('MySQL error:', e)
-          // debug(results)
-          resolve(results[0])
-        })
-    })
+  get (id) { return savedUsers[id] },
+
+  getByUsername (username) {
+    return savedUsers[
+      Object.keys(savedUsers)
+        .find(key => savedUsers[key].username === username)
+    ]
   },
 
-  getUserLocation (userId) {
-    debug('getUserLocation')
-    return new Promise((resolve, reject) => {
-      mysql.query(
-        `SELECT location FROM user
-        WHERE id = '${userId}';`,
-        (e, results, fields) => {
-          if (e) return err('MySQL error:', e)
-          debug(results)
-          resolve(results[0])
-        })
-    })
-  },
+  getAll () { return savedUsers },
 
-  getUserByUsername(username, serverId) {
-    debug('getUserByUsername')
-    return new Promise((resolve, reject) => {
-      mysql.query(
-        `SELECT server_user.lastSeen, user.location, place.lat, place.lng
-        FROM server_user
-        LEFT JOIN user ON user.id = server_user.userId
-        LEFT JOIN place ON place.name = user.location
-        WHERE serverId = '${serverId}' AND username = '${username}';`,
-        (e, results, fields) => {
-          if (e) return err('MySQL error:', e)
-          debug(results)
-          resolve(results[0])
-        })
-    })
-  },
-
-  getUsersInServer(serverId) {
-    debug('getUsersInServer')
-    return new Promise((resolve, reject) => {
-      mysql.query(
-        `SELECT server_user.username, server_user.userId AS id, server_user.lastSeen, user.location, place.lat, place.lng
-        FROM server_user
-        LEFT JOIN user ON user.id = server_user.userId
-        LEFT JOIN place ON place.name = user.location
-        WHERE serverId = '${serverId}';`,
-        (e, results, fields) => {
-          if (e) err('MySQL error:', e)
-          debug(results)
-          resolve(results)
-        })
-    })
-  },
-
-  updateUser (userId, serverId, settings) {
-    debug('updateUser', userId, serverId, settings)
-    return new Promise((resolve, reject) => {
-      // null is a trigger to unset a user's location
-      if (settings.location || settings.location === null)
-        mysql.query(
-          `INSERT INTO user (id, location)
-          VALUES ('${userId}', ${settings.location == null ? null : `'${settings.location}'`})
-          ON DUPLICATE KEY
-          UPDATE 
-            location = ${settings.location == null ? null : `'${settings.location}'`};`,
-          (e, results, fields) => {
-            if (e) err('MySQL error:', e)
-            if (results.message) debug(results)
-          })
-      mysql.query(
-        `INSERT INTO server_user (lastSeen, username, serverId, userId)
-        VALUES (
-          '${new Date()}', 
-          '${settings.username || null}', 
-          '${serverId}', 
-          '${userId}')
-        ON DUPLICATE KEY
-        UPDATE 
-          lastSeen = '${new Date()}',
-          ${ settings.username ?
-            `username = '${settings.username}'` : ''
-          };`,
-        (e, results, fields) => {
-          if (e) err('MySQL error:', e)
-          if (results.message) debug(results)
-          resolve(results)
-        })
-    })
-  },
-
-  getUserLastSeenInServer (userId, serverId) {
-    debug('getUserLastSeen')
-    return new Promise((resolve, reject) => {
-      mysql.query(
-        `SELECT lastSeen FROM server_user
-        WHERE serverId = '${serverId}' AND userId = '${userId}';`,
-        (e, results, fields) => {
-          if (e) err('MySQL error:', e)
-          resolve(results[0] ? results[0].lastSeen : null)
-        })
-    })
-  },
-
-  getCoords (location) {
-    debug('getCoords')
-    return new Promise((resolve, reject) => {
-      mysql.query(
-        `SELECT lat, lng FROM place
-        WHERE name = '${location.toLowerCase()}';`,
-        (e, results, fields) => {
-          if (e) err('MySQL error:', e)
-          resolve(results[0] ? [results[0].lat, results[0].lng] : null)
-        })
-    })
-  },
-
-  setCoords(location, coords) {
-    debug('setCoords')
-    mysql.query(
-      `INSERT INTO place (name, lat, lng)
-      VALUES (
-        '${location.toLowerCase()}', 
-        '${coords[0]}', 
-        '${coords[1]}')
-      ON DUPLICATE KEY
-      UPDATE
-        lat = '${coords[0]}', 
-        lng = '${coords[1]}';`,
-      (e, results, fields) => {
-        if (e) err('MySQL error:', e)
-        if (results.message) debug(results)
+  timezonesIn (serverOrChannelObject) {
+    let relevantTimezones = []
+    const userIdsInCurrentServer = serverOrChannelObject.recipient
+      ? [serverOrChannelObject.recipient.id]
+      : serverOrChannelObject.members.keyArray()
+    Object.keys(savedUsers)
+      .map(k => {
+        if (
+          userIdsInCurrentServer.find(i => i === k)
+          && !relevantTimezones.find(z => z.timezoneName === savedUsers[k].timezoneName)
+        ) {
+          relevantTimezones.push(savedUsers[k])
+        }
       })
+    return relevantTimezones.sort((a, b) => a.offset > b.offset)
   },
+
+  update (id, settings) {
+    savedUsers[id] = savedUsers[id] || {}
+    for (let prop in settings)
+      savedUsers[id][prop] = settings[prop]
+    savedUsers[id].lastSeen = new Date()
+    fs.writeFile("./data/users.json", JSON.stringify(savedUsers), 'utf8', e => {
+      if (e) return console.log(e)
+    })
+    // console.log('Updated user', id)
+    return savedUsers[id]
+  },
+
+  lastSeen (id) {
+    return savedUsers[id] ? savedUsers[id].lastSeen : undefined
+  }
 
 }
--- scripts/interact.js
+++ scripts/interact.js
@@ -2,11 +2,6 @@ const db = require('./db')
 const format = require('./format')
 const get = require('./get')
 
-const logger = require('./log.js')
-const log = logger('interact', 'blue')
-const err = logger('interact', 'red')
-const debug = logger('interact', 'blue', true)
-
 const MINIMUM_LAST_SEEN_TIME_SPAN = 2 * 60 * 60 * 1000 // first number is hours
 const MINIMUM_TIMEZONE_DIFFERENCE = 3 // hours
 
@@ -20,54 +15,36 @@ module.exports = {
 \`!help\` to show this message.`)
   },
 
-  async at (msg, atsInMessage) {
-    debug('at')
-    const serverId = get.serverId(msg)
-    const senderTimezoneOffset = await get.userFullData(msg.author.id, serverId).offset
+  at (msg, atsInMessage, senderTimezoneOffset) {
     if (!senderTimezoneOffset) return
     for (let id of atsInMessage) {
-      const userInfo = await get.userFullData(id, serverId)
-      debug('interact.at', userInfo)
+      const userInfo = db.get(id)
       if (!userInfo || !userInfo.lastSeen) continue
       const msSince = Date.now() - new Date(userInfo.lastSeen).getTime()
       if (
         msSince >= MINIMUM_LAST_SEEN_TIME_SPAN
         && Math.abs(senderTimezoneOffset - userInfo.offset) >= MINIMUM_TIMEZONE_DIFFERENCE
       ) {
-        db.updateUser(id)
+        db.update(id)
         msg.channel.send(`\`It's ${format.currentTimeAt(userInfo.location)} for ${userInfo.username}. (${userInfo.timezoneName})\``)
       }
     }
   },
 
-  async me(msg, senderUsername) {
-    debug('me')
-    const userInfo = await get.userFullData(msg.author.id, get.serverId(msg))
-    if (!userInfo.offset)
+  me (msg, senderUsername) {
+    const userInfo = db.get(msg.author.id)
+    if (!userInfo)
       return msg.channel.send(`No timezone has been set for ${senderUsername}.`)
     msg.channel.send(`\`It's ${format.currentTimeAt(userInfo.location)} for ${userInfo.username}. (${userInfo.timezoneName})\``)
   },
 
-  unset(msg, senderUsername) {
-    debug('unset')
-    db.updateUser(msg.author.id, get.serverId(msg), {
-      location: null,
-      username: senderUsername
-    })
-    msg.channel.send(`Unset time zone for ${senderUsername}.`)
-  },
-
-  async set(msg, senderUsername) {
-    debug('set')
+  async set (msg) {
     const regex = /!set (.*)/g
     const location = regex.exec(msg.content)
     if (location && location[1]) {
-      db.updateUser(msg.author.id, get.serverId(msg), {
-        username: senderUsername,
-        location: location[1]
-      })
-      const foundTimezone = await get.timezoneFromLocation({ location: location[1] })
+      const foundTimezone = await get.timezoneFromLocation(location[1])
       if (foundTimezone) {
+        db.update(msg.author.id, { ...foundTimezone, username: msg.author.username })
         msg.channel.send(`Time zone for ${msg.author.username} set to ${foundTimezone.timezoneName}.`)
       }
       else
@@ -77,68 +54,58 @@ module.exports = {
       msg.channel.send(`Use this command in the format \`!set <city or country name>\` to set your timezone.`)
   },
 
-  async timeAt(msg) {
-    debug('timeAt')
+  async timeAt (msg) {
     const regex = /!time (.*)/g
     const searchString = regex.exec(msg.content)
-    if (!searchString || !searchString[1]) {
+    if (!searchString) {
       msg.channel.send(`Use this command in the format \`!time <user, city, or country name>\` to see the current time there.`)
       return
     }
-
-    // first, assume it's a username and compare it to the users in the server
     const users = msg.guild ?
       msg.guild.members.map(m => ({ username: m.nickname })) :
       [{ username: msg.author.username }]
-    let foundUserInServer = await get.userInMessage(searchString[1], users)
-
-    if (foundUserInServer && foundUserInServer.location) {
-      const { location, lat, lng } = foundUserInServer
-      const foundTimezone = get.timezoneFromLocation(location, lat, lng)
+    let foundUsername = get.usernameInMessage(searchString[1], users)
+    if (foundUsername) {
+      const foundTimezone = db.getByUsername(foundUsername)
+      if (!foundTimezone)
+        return msg.channel.send(`No timezone listed for user ${foundUsername}.`)
       msg.channel.send(`\`It's ${format.currentTimeAt(foundTimezone.location)} for ${foundTimezone.username}. (${foundTimezone.timezoneName})\``)
     }
-    else if (foundUserInServer)
-      return msg.channel.send(`No timezone listed for user ${foundUsername}.`)
-
-    // then, take a swing at it being a location
     else {
-      const foundTimezone = await get.timezoneFromLocation({
-        location: searchString[1]
-      })
+      const foundTimezone = await get.timezoneFromLocation(searchString[1])
       if (!foundTimezone)
         return msg.channel.send(`No timezone found for ${searchString[1]}.`)
       msg.channel.send(`\`\`\`${format.timezone(foundTimezone)}\`\`\``)
     }
   },
 
-  async listUsers(msg) {
-    debug('listUsers')
-    let savedUsers
-  
-    if (msg.guild)
-      savedUsers = await db.getUsersInServer(get.serverId(msg))
-    else
-      savedUsers = [await db.getUser(msg.author.id, get.serverId(msg))]
+  listUsers (msg) {
+    const allUsers = db.getAll()
+    const timezonesWithUsers = Object.values(allUsers)
+      .sort((a, b) => a.offset > b.offset)
+      .reduce((acc, user) => {
+        const timezoneName = user.timezoneName
+        if(!acc[timezoneName]) {
+          acc[timezoneName] = {
+            locale: user.location,
+            label: `${user.timezoneName} (UTC ${user.offset >= 0 ? '+' : ''}${user.offset})`,
+            usernames: []
+          }
+        }
+        acc[timezoneName].usernames.push(user.username)
+        return acc
+      }, {})
 
-    const allUsersTimezonePromises = []
-    for (let user of savedUsers) {
-      if (user.location || user.lat || user.lng)
-        allUsersTimezonePromises.push({
-          ...(await get.timezoneFromLocation({ 
-            location: user.location,
-            lat: user.lat,
-            lng: user.lng
-          })),
-          username: user.username
-        })
-    }
-    
-    Promise.all(allUsersTimezonePromises)
-      .then(allUsersTimezones => {
-        const outputString = format.usersByTimezone(allUsersTimezones)
-        if (!outputString)
-          return msg.channel.send(`No users in this server have added their timezone yet. Use \`!set <city or country name>\` to set your timezone.`)
-        msg.channel.send(`\`\`\`${outputString}\`\`\``)
-      })
+    const outputString = Object.values(timezonesWithUsers)
+      .reduce((acc, timezone) => {
+        const header = `${format.currentTimeAt(timezone.locale, true)} - ${timezone.label}`
+        const body = '\n  ' + timezone.usernames.join('\n  ') + '\n\n'
+        return acc + header + body
+      }, '')
+
+    if (!outputString)
+      return msg.channel.send(`No users in this server have added their timezone yet. Use \`!set <city or country name>\` to set your timezone.`)
+
+    msg.channel.send(`\`\`\`${outputString}\`\`\``)
   }
 }
