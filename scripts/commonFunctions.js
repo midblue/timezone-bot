421d60aca307b96162adf74ee9f1f4945ec8998b
Jasper Stephenson <jasperstephenson@gmail.com>
2020-07-10 18:14:44 +0900

    missed ANOTHER await

--- scripts/commonFunctions.js
+++ scripts/commonFunctions.js
@@ -39,7 +39,7 @@ module.exports = {
   async getContactsOrOwnerOrModerator({ guild }) {
     let usersToContact
     // check guild.owner
-    usersToContact = getUserInGuildFromId(guild, guild.ownerID)
+    usersToContact = await getUserInGuildFromId(guild, guild.ownerID)
     if (usersToContact) return [usersToContact]
     // at this point, we just look for an admin of any kind
     usersToContact = (await getGuildMembers({ guild })).filter(member =>
