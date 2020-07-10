a0b07b485c71f7b5ab27de294bd589719732e00b
Jasper Stephenson <jasperstephenson@gmail.com>
2020-07-10 10:55:57 +0900

    minor logging change

--- events/kickedFromServer.js
+++ events/kickedFromServer.js
@@ -3,5 +3,5 @@
 module.exports = guild => {
   // seems like there's no reason to delete their settings, they might readd later
   // db.removeGuild({ guildId: guild.id })
-  console.log('Was removed from a guild: ' + guild.name)
+  console.log('< < < < < <           Was removed from a guild: ' + guild.name)
 }
