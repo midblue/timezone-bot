a31a8416f5e110da7e4cd6aa7992f9ef57052a63
Jasper Stephenson <jasperstephenson@gmail.com>
2020-07-10 19:43:58 +0900

    improved logging

--- actions/contactGuildAdmin.js
+++ actions/contactGuildAdmin.js
@@ -11,12 +11,17 @@ module.exports = async ({ guild, message }) => {
   if (!currentGuildContacts)
     return console.log('Failed to find contact points in server', guild.name)
   currentGuildContacts.forEach(singleContact =>
-    singleContact.user.send(message.substring(0, 1999)).catch(err => {
-      console.log(
-        `Failed to contact admin ${getLabelFromUser(singleContact)}: ${
-          err.message
-        }`,
-      )
-    }),
+    singleContact.user
+      .send(message.substring(0, 1999))
+      .then(() => {
+        console.log('Contacted admin', singleContact.user.username)
+      })
+      .catch(err => {
+        console.log(
+          `Failed to contact admin ${getLabelFromUser(singleContact)}: ${
+            err.message
+          }`,
+        )
+      }),
   )
 }
