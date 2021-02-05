4ac2be90e62b28c629a21210a1e9dcc8595aaf36
Jasper Stephenson <jasperstephenson@gmail.com>
2021-02-05 12:25:30 +0100

    testing greatly increased memo size to help with db usage

--- db/guild.js
+++ db/guild.js
@@ -1,7 +1,7 @@
 const defaultServerSettings = require('../scripts/defaultServerSettings')
 const memo = require('../scripts/memo')
 
-const memoedGuildData = memo(100)
+const memoedGuildData = memo(500)
 let firestore
 
 module.exports = function (passedFirestore) {
